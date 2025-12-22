0aadb5f69c6ebb2957bddc22611edd8f74d59e86
james5635 <brorojame2029@gmail.com>
2025-12-22 03:53:57 +0000

    update

--- flake.nix
+++ flake.nix
@@ -1,36 +1,49 @@
 {
-description = "Flutter 3.13.x";
-inputs = {
-  nixpkgs.url = "github:NixOS/nixpkgs/25.11";
-  flake-utils.url = "github:numtide/flake-utils";
-};
-outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          android_sdk.accept_license = true;
-          allowUnfree = true;
+  description = "Flutter 3.13.x";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            android_sdk.accept_license = true;
+            allowUnfree = true;
+          };
         };
-      };
-      buildToolsVersion = "34.0.0";
-      androidComposition = pkgs.androidenv.composeAndroidPackages {
-        buildToolsVersions = [ buildToolsVersion "28.0.3" ];
-        platformVersions = [ "34" "28" ];
-        abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
-      };
-      androidSdk = androidComposition.androidsdk;
-    in
-    {
-      devShell =
-        with pkgs; mkShell rec {
+
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          buildToolsVersions = [ "34.0.0" "28.0.3" ];
+          platformVersions   = [ "34" "28" ];
+          abiVersions        = [ "armeabi-v7a" "arm64-v8a" ];
+
+          includeEmulator    = true;
+          includeSystemImages = true;
+
+          systemImageTypes = [ "google_apis_playstore" ];
+        };
+
+        androidSdk = androidComposition.androidsdk;
+
+      in {
+        devShell = pkgs.mkShell {
           ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
-          buildInputs = [
+          ANDROID_HOME     = "${androidSdk}/libexec/android-sdk";
+
+          buildInputs = with pkgs; [
             flutter
-            androidSdk # The customized SDK that we've made above
+            androidSdk
             jdk17
           ];
+
+          shellHook = ''
+            export PATH=$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools:$PATH
+          '';
         };
-    });
+      });
 }
