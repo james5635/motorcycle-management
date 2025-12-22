d8798f7cb3d01522e1af345942c63e98bcdbdfd1
james5635 <brorojame2029@gmail.com>
2025-12-22 18:34:39 +0700

    update

--- .devcontainer/nix.sh
+++ .devcontainer/nix.sh
@@ -4,3 +4,24 @@ sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daem
 
 # source it
 echo "source ~/.nix-profile/etc/profile.d/nix.sh" >> ~/.bashrc
+
+# ERROR:
+# -----------------------------------------------------------------------------
+# @james5635 ? /workspaces/motorcycle-management (main) $ flutter run -d linux
+# Launching lib/main.dart on Linux in debug mode...
+# Building Linux application...                                           
+# ? Built build/linux/x64/debug/bundle/motorcycle_management
+# No provider of eglGetPlatformDisplayEXT found.  Requires one of:
+#     EGL_EXT_platform_base
+# Error waiting for a debug connection: The log reader stopped unexpectedly, or
+# never started.
+# Error launching application on Linux.
+# -----------------------------------------------------------------------------
+#
+# SOLUTION:
+# -----------------------------------------------------------------------------
+# sudo apt-get update -y && sudo apt-get upgrade -y
+# sudo apt-get install -y curl git unzip xz-utils zip libglu1-mesa
+# -----------------------------------------------------------------------------
+#
+# NOTE: libglu1-mesa is important
