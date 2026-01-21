9fef8cf5737e5826ef991c521bfdc6746845c691
james5635 <brorojame2029@gmail.com>
2026-01-21 12:21:38 +0000

    verify login

--- backend/src/main/java/com/example/demo/DatabaseSeeder.java
+++ backend/src/main/java/com/example/demo/DatabaseSeeder.java
@@ -1,41 +1,39 @@
-// package com.example.demo;
+package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.CommandLineRunner;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.stereotype.Component;
+@Component
+public class DatabaseSeeder implements CommandLineRunner {
 
+    private final UserService userService;
 
-// @Component
-// public class DatabaseSeeder implements CommandLineRunner {
+    private final PasswordEncoder encoder;
 
-//     private final UserService userService;
+    @Autowired
+    public DatabaseSeeder(UserService userService, PasswordEncoder encoder) {
+        this.userService = userService;
+        this.encoder = encoder;
+    }
 
-//     // private final PasswordEncoder encoder;
+    @Override
+    public void run(String... args) {
+        if (userService.count() == 0) {
+            User u1 = User.builder().fullName("jame").email("jame@gamil.com")
+                    .passwordHash(encoder.encode("hello"))
+                    .phoneNumber("2213123").address("phnom penh").role("Customer")
+                    .profileImageUrl("https://aaa.comss")
+                    .build();
+            User u2 = User.builder().fullName("jonh").email("jonh@gmail.com")
+                    .passwordHash(encoder.encode("hello"))
+                    .phoneNumber("2213123").address("phnom penh").role("Customer")
+                    .profileImageUrl("https://aaa.comas")
+                    .build();
+            userService.save(u1);
+            userService.save(u2);
 
-//     @Autowired
-//     public DatabaseSeeder(UserService userService) {
-//         this.userService = userService;
-//         // this.encoder = encoder;
-//     }
-
-//     @Override
-//     public void run(String... args) {
-//         if (userService.count() == 0) {
-//             User u1 = User.builder().fullName("jame").email("jame@gamil.com")
-//                     .passwordHash(encoder.encode("hello"))
-//                     .phoneNumber("2213123").address("phnom penh").role("Customer")
-//                     .profileImageUrl("https://aaa.comss")
-//                     .build();
-//             User u2 = User.builder().fullName("jonh").email("jonh@gmail.com")
-//                     .passwordHash(encoder.encode("hello"))
-//                     .phoneNumber("2213123").address("phnom penh").role("Customer")
-//                     .profileImageUrl("https://aaa.comas")
-//                     .build();
-//             // userService.save(u1);
-//             // userService.save(u2);
-
-//         }
-//     }
-// }
+        }
+    }
+}
